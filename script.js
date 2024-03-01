ea0adfd6177f346ce97ce467222925f2e024f071
KlaudiaWww <klaudia.witczak.fm@gmail.com>
2024-03-01 20:27:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,151 @@
+function myFunction() {
+    const element = document.getElementById("id01");
+    element.innerHTML = "Nowy nagłówek";
+}
+
+function myColorChange() {
+    const element = document.getElementById("id01");
+    element.style.color = "red";
+}
+
+
+
+
+
+//"JavaScript is awesome".length;
+//"JavaScript is awesome".toUpperCase();
+//"JavaScript is awesome".toLowerCase();
+
+
+
+
+
+
+//const message = "JavaScript is awesome!";
+//alert(message);
+
+
+
+
+//const imie = prompt("Podaj swoje imie:");
+//console.log("Witaj " + imie + "!");
+
+
+
+
+
+// const value = prompt("Podaj cyfre:");
+// console.log(typeof(value)); 
+// console.log(value); 
+
+
+
+
+
+// function greet() {
+//     let a = 'hello';
+
+//     // b niedostepne
+//     if(a == 'hello'){
+//         // b dostepne
+//         let b = 'world';
+//         console.log(a + ' ' + b);
+//     }
+
+//      // b niedostepne
+//     console.log(a + ' ' + b); // error
+// }
+// // a niedostepne
+
+// greet();
+
+
+
+
+
+
+
+
+// var a = 5; // 5
+// var a = 3; // 3
+// console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = 5;
+// a = 3; // error 
+// console.log(a);
+
+
+
+
+
+
+
+
+
+// var a = 5;
+// console.log(a); // 5
+// {
+//     var a = 3;
+//     console.log(a); // 3
+// }
+// console.log(a); // 3
+
+
+
+
+
+
+
+
+
+// let a = 5;
+// console.log(a); // 5
+// {
+//     let a = 3;
+//     console.log(a); // 3
+// }
+// console.log(a); // 5
+
+
+
+
+
+
+
+
+
+
+// var a = 2;
+// for(var a = 0; a < 3; a++) {
+//     console.log('hello');
+// }
+// console.log(a); // 3
+
+
+
+
+
+
+
+
+
+
+
+// let a = 2;
+// for(let a = 0; a < 3; a++) {
+//     console.log('hello');
+// }
+// console.log(a); // 2
+
+
